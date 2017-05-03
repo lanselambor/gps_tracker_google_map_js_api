9026abfea5d260ec49b36d41a11df58b5eb3b822
lanselambor <757016515@qq.com>
2017-05-03 10:30:32 +0800

    Added realtime restful server

--- app.js
+++ app.js
@@ -1,27 +1,100 @@
-var express = require('express');
-var app = express();
+// server.js
+
+// BASE SETUP
+// =============================================================================
+
+// var Bear     = require('./app/models/bear');
+
+// var mongoose   = require('mongoose');
+// mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o'); // connect to our database
+
+
+
+// call the packages we need
+var express    = require('express');        // call express
+var app        = express();                 // define our app using express
+var bodyParser = require('body-parser');
 var path = require('path');
 
-app.use(express.static(path.join(__dirname, 'public')))
+// global variable
+var window_lat = 0.0;
+var window_lng = 0.0;
+var window_lat_old = 0.0;
+var window_lng_old = 0.0;
 
-var server = app.listen(3000, function () {
-    var host = server.address().address;
-    var port = server.address().port;
+app.use(express.static(path.join(__dirname, 'public')));
 
-    console.log('Example app listening at http://%s:%s', host, port);
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3001;        // set our port
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+// middleware to use for all requests
+// router.use(function(req, res, next) {
+//     // do logging
+//     var content = req.body.name;
+//     console.log("Received: " + content);
+//     res.json({ message: 'Bear created!' });
+//     // console.log('Something is happening.');
+//     next(); // make sure we go to the next routes and don't stop here
+// });
+
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+// router.get('/', function(req, res) {
+//     res.json({ message: 'hooray! welcome to our api!' });
+// });
+
+
+// on routes that end in /bears
+// ----------------------------------------------------
+router.route('/latlng');
+
+// Deal with POST event
+router.route('/latlng/:data').post(function(req, res) {
+        var str = req.params.data.toString();
+        console.log(str);
+        var latlng = str.split("&");
+        window_lat = Number(latlng[0])/10000000;
+        window_lng = Number(latlng[1])/10000000;
+        var date = new Date();
+        console.log(date + ": ");
+        console.log("lat: " + window_lat.toString());
+        console.log("lng: " + window_lng.toString());
+        // res.json({message: 'Return: ' + window_lat + ", " +window_lng});
+        res.send(date + ":\r\n" + window_lat + ", " +window_lng + "\r\nOK\r\n");
 });
+
+
+
+// more routes for our API will happen here
+
+// REGISTER OUR ROUTES -------------------------------
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+var server = app.listen(port);
 
 var io = require('socket.io').listen(server);
-var coor_lat = 22.584358;
-var coor_lng = 113.96666;
-
 io.sockets.on('connection', function (socket) {
-    console.log("A new user connected!");
-    setInterval(function(){
-        coor_lat += (Math.random() / 5000.0);
-        coor_lng += (Math.random() / 5000.0);
-        socket.broadcast.emit("marker",{'lat': coor_lat, 'lng': coor_lng});
-        var date = Date();
-        console.log("Emited new coordinate!" + " : " + date);
-    }, 2000);
+        console.log("A new user connected!");
+        setInterval(function(){
+                if(window_lat_old != window_lat || window_lng_old != window_lng){
+                        socket.broadcast.emit("marker",{'lat': window_lat, 'lng': window_lng});
+                        var date = new Date();
+                        console.log("Emited new coordinate!" + " : " + date);
+                }
+                window_lat_old = window_lat;
+                window_lng_old = window_lng;
+        }, 2000);
 });
+
+console.log('Magic happens on port ' + port);
